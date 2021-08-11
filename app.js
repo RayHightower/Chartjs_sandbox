f6296113303cbcae32c93348e2278b129546f156
Raymond T. Hightower <rth@rayhightower.com>
2021-08-10 21:10:55 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,12 @@
+let ctx1=document.getElementById("myBarChart").getContext("2d");
+
+let myBarChart=new Chart(ctx1,{
+    type:"bar",
+    data:{
+        labels:["Jan", "Feb", "Mar"],
+        datasets:[{
+            label:"Temperature",
+            data:[12, 15, 18]
+        }]
+    }
+});
