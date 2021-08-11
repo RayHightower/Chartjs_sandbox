0f6d828b37de3210f7ebcff571a92722bd318685
Raymond T. Hightower <rth@rayhightower.com>
2021-08-10 21:30:00 -0700

    Add a pie chart with the same dataset as the bar chart.

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 let ctx1=document.getElementById("myBarChart").getContext("2d");
+let ctx2=document.getElementById("myPieChart").getContext("2d");
 
 let myBarChart=new Chart(ctx1,{
     type:"bar",
+    data:{
+        labels:["Jan", "Feb", "Mar"],
+        datasets:[{
+            label:"Temperature",
+            data:[12, 15, 18],
+            backgroundColor:["red", "green", "blue"]
+        }]
+    },
+    options:{
+        responsive:false,
+        scales:{
+            yAxes:[{
+                ticks:{
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+let myPieChart=new Chart(ctx2,{
+    type:"pie",
     data:{
         labels:["Jan", "Feb", "Mar"],
         datasets:[{
