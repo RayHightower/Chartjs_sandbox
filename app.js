01b4338ef8484f7a5475a335f8c15207f9df6391
Raymond T. Hightower <rth@rayhightower.com>
2021-08-17 22:12:15 -0700

    Add a doughnut chart. Doughnut charts are cooler than pie charts!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let ctx1=document.getElementById("myBarChart").getContext("2d");
 let ctx2=document.getElementById("myPieChart").getContext("2d");
+let ctx3=document.getElementById("myDoughnutChart").getContext("2d");
 
 let myBarChart=new Chart(ctx1,{
     type:"bar",
@@ -25,6 +26,28 @@ let myBarChart=new Chart(ctx1,{
 
 let myPieChart=new Chart(ctx2,{
     type:"pie",
+    data:{
+        labels:["Jan", "Feb", "Mar"],
+        datasets:[{
+            label:"Temperature",
+            data:[12, 15, 18],
+            backgroundColor:["red", "green", "blue"]
+        }]
+    },
+    options:{
+        responsive:false,
+        scales:{
+            yAxes:[{
+                ticks:{
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+let myDoughnutChart=new Chart(ctx3,{
+    type:"doughnut",
     data:{
         labels:["Jan", "Feb", "Mar"],
         datasets:[{
