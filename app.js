d1496a83b151eb99a50649efe445d9e52bf3ef88
Raymond T. Hightower <rth@rayhightower.com>
2021-08-18 22:52:39 -0700

    Add a line chart. Explore strange behavior between index.html and app.js.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let ctx1=document.getElementById("myBarChart").getContext("2d");
 let ctx2=document.getElementById("myPieChart").getContext("2d");
 let ctx3=document.getElementById("myDoughnutChart").getContext("2d");
+let ctx4=document.getElementById("myLineChart").getContext("2d");
 
 let myBarChart=new Chart(ctx1,{
     type:"bar",
@@ -48,6 +49,28 @@ let myPieChart=new Chart(ctx2,{
 
 let myDoughnutChart=new Chart(ctx3,{
     type:"doughnut",
+    data:{
+        labels:["Jan", "Feb", "Mar"],
+        datasets:[{
+            label:"Temperature",
+            data:[12, 15, 18],
+            backgroundColor:["red", "green", "blue"]
+        }]
+    },
+    options:{
+        responsive:false,
+        scales:{
+            yAxes:[{
+                ticks:{
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+let myLineChart=new Chart(ctx4,{
+    type:"line",
     data:{
         labels:["Jan", "Feb", "Mar"],
         datasets:[{
