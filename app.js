45ec571c2cff533b0a63969449a4fd86614d3372
Raymond T. Hightower <rth@rayhightower.com>
2021-08-26 17:00:49 -0700

    Try alpha adjustments on the doughnut chart.

--- app.js
+++ app.js
@@ -56,7 +56,15 @@ let myDoughnutChart=new Chart(ctx3,{
         datasets:[{
             label:"Market Share",
             data:[12, 15, 18, 2, 23, 4],
-            backgroundColor:["red", "green", "blue", "orange", "yellow", "turquoise"]
+            backgroundColor:[
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            /* backgroundColor:["red", "green", "blue", "orange", "yellow", "turquoise"] */
         }]
     },
     options:{
